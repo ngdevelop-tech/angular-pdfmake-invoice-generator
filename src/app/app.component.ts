881313e4f7109946035150b996e8081f43546e96
Ankit.P <ankit.p@ezdi.us>
2020-07-11 22:33:52 +0530

    Simple PDF Generated with PDFMake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-pdfmake-invoice';
+
+  generatePDF() {
+    let docDefinition = {
+      header: 'C#Corner PDF Header',
+      content: 'Sample PDF generated with Angular and PDFMake for C#Corner Blog'
+    };
+
+    pdfMake.createPdf(docDefinition).open();
+  }
 }
