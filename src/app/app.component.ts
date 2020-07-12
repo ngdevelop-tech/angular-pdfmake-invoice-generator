2784e35be5b22ddb168c9219046f58534feef593
Ankit.P <ankit.p@ezdi.us>
2020-07-12 18:49:02 +0530

    Invoice Generator Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,141 @@ import { Component } from '@angular/core';
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+class Product{
+  name: string;
+  price: number;
+  qty: number;
+}
+class Invoice{
+  customerName: string;
+  address: string;
+  contactNo: number;
+  email: string;
+  
+  products: Product[] = [];
+  additionalDetails: string;
+
+  constructor(){
+    // Initially one empty product row we will show 
+    this.products.push(new Product());
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pdfmake-invoice';
-
-  generatePDF() {
+  invoice = new Invoice(); 
+  
+  generatePDF(action = 'open') {
     let docDefinition = {
-      header: 'C#Corner PDF Header',
-      content: 'Sample PDF generated with Angular and PDFMake for C#Corner Blog'
+      content: [
+        {
+          text: 'ELECTRONIC SHOP',
+          fontSize: 16,
+          alignment: 'center',
+          color: '#047886'
+        },
+        {
+          text: 'INVOICE',
+          fontSize: 20,
+          bold: true,
+          alignment: 'center',
+          decoration: 'underline',
+          color: 'skyblue'
+        },
+        {
+          text: 'Customer Details',
+          style: 'sectionHeader'
+        },
+        {
+          columns: [
+            [
+              {
+                text: this.invoice.customerName,
+                bold:true
+              },
+              { text: this.invoice.address },
+              { text: this.invoice.email },
+              { text: this.invoice.contactNo }
+            ],
+            [
+              {
+                text: `Date: ${new Date().toLocaleString()}`,
+                alignment: 'right'
+              },
+              { 
+                text: `Bill No : ${((Math.random() *1000).toFixed(0))}`,
+                alignment: 'right'
+              }
+            ]
+          ]
+        },
+        {
+          text: 'Order Details',
+          style: 'sectionHeader'
+        },
+        {
+          table: {
+            headerRows: 1,
+            widths: ['*', 'auto', 'auto', 'auto'],
+            body: [
+              ['Product', 'Price', 'Quantity', 'Amount'],
+              ...this.invoice.products.map(p => ([p.name, p.price, p.qty, (p.price*p.qty).toFixed(2)])),
+              [{text: 'Total Amount', colSpan: 3}, {}, {}, this.invoice.products.reduce((sum, p)=> sum + (p.qty * p.price), 0).toFixed(2)]
+            ]
+          }
+        },
+        {
+          text: 'Additional Details',
+          style: 'sectionHeader'
+        },
+        {
+            text: this.invoice.additionalDetails,
+            margin: [0, 0 ,0, 15]          
+        },
+        {
+          columns: [
+            [{ qr: `${this.invoice.customerName}`, fit: '50' }],
+            [{ text: 'Signature', alignment: 'right', italics: true}],
+          ]
+        },
+        {
+          text: 'Terms and Conditions',
+          style: 'sectionHeader'
+        },
+        {
+            ul: [
+              'Order can be return in max 10 days.',
+              'Warrenty of the product will be subject to the manufacturer terms and conditions.',
+              'This is system generated invoice.',
+            ],
+        }
+      ],
+      styles: {
+        sectionHeader: {
+          bold: true,
+          decoration: 'underline',
+          fontSize: 14,
+          margin: [0, 15,0, 15]          
+        }
+      }
     };
 
-    pdfMake.createPdf(docDefinition).open();
+    if(action==='download'){
+      pdfMake.createPdf(docDefinition).download();
+    }else if(action === 'print'){
+      pdfMake.createPdf(docDefinition).print();      
+    }else{
+      pdfMake.createPdf(docDefinition).open();      
+    }
+
   }
+
+  addProduct(){
+    this.invoice.products.push(new Product());
+  }
+  
 }
